f5395f6f221129af30ab7ce0847df04f09c505bf
bufan <821869798@qq.com>
2026-09-04 18:21:20 +0800

    fix: use Perry native Hono server adapter

--- src/platforms/perry-server.js
+++ src/platforms/perry-server.js
@@ -1,15 +1,15 @@
+import { serve } from '@perryts/hono-server';
 import { createApp } from '../app/createApp.jsx';
 import { MemoryKVAdapter } from '../adapters/kv/memoryKv.js';
-import { startPerryHttpServer } from './perryHttpServer.js';
 
-console.log('[perry] entry loaded');
-
-const port = 8797;
+const port = 8787;
 const logger = console;
-console.log('[perry] creating runtime');
-const kv = new MemoryKVAdapter();
-console.log('[perry] creating app');
-const app = createApp({ kv, assetFetcher: null, logger });
-console.log('[perry] starting HTTP server');
-startPerryHttpServer(app, { port, logger });
-console.log('[perry] HTTP server started');
+const app = createApp({
+    kv: new MemoryKVAdapter(),
+    assetFetcher: null,
+    logger
+});
+
+serve({ fetch: app.fetch, port }, ({ address }) => {
+    logger.info(`Sublink worker running on ${address}`);
+});
