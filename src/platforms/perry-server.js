8753fd57a518a4641f002356394da52fb43b5478
bufan <821869798@qq.com>
2026-09-04 17:10:56 +0800

    ci: trace Perry startup crash

--- src/platforms/perry-server.js
+++ src/platforms/perry-server.js
@@ -2,12 +2,14 @@ import { createApp } from '../app/createApp.jsx';
 import { MemoryKVAdapter } from '../adapters/kv/memoryKv.js';
 import { startNodeHttpServer } from './nodeHttpServer.js';
 
-const port = Number(process.env.PORT || 8787);
-const logger = console;
-const app = createApp({
-    kv: new MemoryKVAdapter(),
-    assetFetcher: null,
-    logger
-});
+console.log('[perry] entry loaded');
 
+const port = 8797;
+const logger = console;
+console.log('[perry] creating runtime');
+const kv = new MemoryKVAdapter();
+console.log('[perry] creating app');
+const app = createApp({ kv, assetFetcher: null, logger });
+console.log('[perry] starting HTTP server');
 startNodeHttpServer(app, { port, logger });
+console.log('[perry] HTTP server started');
