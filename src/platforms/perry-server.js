9a28d99e61b990d6f684bac2f7656566623f61a8
bufan <821869798@qq.com>
2026-09-04 16:44:52 +0800

    fix: simplify Perry runtime entrypoint

--- src/platforms/perry-server.js
+++ src/platforms/perry-server.js
@@ -1,6 +1,5 @@
 import { createApp } from '../app/createApp.jsx';
 import { MemoryKVAdapter } from '../adapters/kv/memoryKv.js';
-import { spawn } from 'child_process';
 import { startNodeHttpServer } from './nodeHttpServer.js';
 
 const port = Number(process.env.PORT || 8787);
@@ -12,19 +11,3 @@ const app = createApp({
 });
 
 startNodeHttpServer(app, { port, logger });
-
-if (process.env.SUBLINK_NO_OPEN_BROWSER !== 'true') {
-    setTimeout(() => openBrowser(`http://127.0.0.1:${port}`), 500);
-}
-
-function openBrowser(url) {
-    const commands = {
-        win32: ['cmd', ['/c', 'start', '', url]],
-        darwin: ['open', [url]],
-        linux: ['xdg-open', [url]]
-    };
-    const [command, args] = commands[process.platform] || commands.linux;
-    const child = spawn(command, args, { detached: true, stdio: 'ignore' });
-    child.on('error', (error) => logger.warn?.(`Open ${url} manually`, error));
-    child.unref();
-}
